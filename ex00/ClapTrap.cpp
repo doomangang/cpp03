e1d9c2d7c6ee47c5c14e414f4dca1a4b63fe4876
doomangang <sp0943@cau.ac.kr>
2024-11-25 21:36:43 +0900

    01 impl

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,6 +1,6 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap(std::string name)
+ClapTrap::ClapTrap(std::string name) : _hitPoints(10), _energyPoints(10), _attackDamage(0)
 {
 	std::cout << "Default Constructor Called\n";
 	_name = name;
@@ -19,21 +19,48 @@ ClapTrap::~ClapTrap()
 
 ClapTrap&   ClapTrap::operator=(const ClapTrap& other)
 {
-	std::cout << "Copy assignment operator called\n" + other.getName() + "'s to be copied";
-	this->_name = other.getName();
-	this->_hitPoints = other.getHitPoints();
-	this->_energyPoints = other.getEnergyPoints();
-	this->_attackDamage = other.getAttackDamage();
+	std::cout << "Copy assignment operator called\n" + other.getName() + "'s to be copied\n";
+	if (*this != other)
+	{
+		this->_name = other.getName();
+		this->_hitPoints = other.getHitPoints();
+		this->_energyPoints = other.getEnergyPoints();
+		this->_attackDamage = other.getAttackDamage();
+	}
 	return (*this);
 }
 
 void        ClapTrap::attack(const std::string& target)
 {
-
+	if (_hitPoints <= 0 || _energyPoints <= 0){
+		std::cout << "ClapTrap" + _name + "is out of hit point or energy point, couldn't attack\n";
+		return ;
+	}
+	std::cout << "ClapTrap " + _name + " attacks " + target + ", causing " + _attackDamage + " points of damage!\n";
+	_energyPoints--;
 }
 
-void        ClapTrap::takeDamage(unsigned int amount);
-void        ClapTrap::beRepaired(unsigned int amount);
+void        ClapTrap::takeDamage(unsigned int amount)
+{
+	std::cout << "ClapTrap " + _name + "took damage, losing " + amount + " amount of hit points\n";
+	if (_hitPoints < INT32_MIN + amount)
+		_hitPoints = INT32_MIN;
+	else
+		_hitPoints -= amount;
+}
+void        ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_hitPoints <= 0 || _energyPoints <= 0){
+		std::cout << "ClapTrap " + _name + "is out of hit point or energy point, couldn't be repaired\n";
+		return ;
+	}
+	std::cout << "ClapTrap " + _name + "has been repaired " + amount + " amount of hit points\n";
+	_energyPoints--;
+	if (_hitPoints > INT32_MAX - amount)
+		_hitPoints = INT32_MAX;
+	else
+		_hitPoints += amount;
+}
 
 std::string ClapTrap::getName()
 {
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -3,9 +3,9 @@
 class ClapTrap{
 private:
 	std::string _name;
-	int         _hitPoints = 10;
-	int         _energyPoints = 10;
-	int         _attackDamage = 0;
+	int         _hitPoints;
+	int         _energyPoints;
+	int         _attackDamage;
 public:
 	ClapTrap(std::string);
 	ClapTrap(const ClapTrap& other);
@@ -21,3 +21,10 @@ public:
 	std::string getEnergyPoints();
 	std::string getAttackDamage();
 };
+
+/*
+ * Attack : target loses <attack damage> hit points
+ * Repair : <amount> hit points back
+ * Energy point : costs 1 by each attacking and repairing
+ * Can't do anything if no Hit Point or energy points left
+ */
